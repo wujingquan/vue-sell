aa75642dc99cc9ca7e4293e9d599790ed25d5caf
touch_co <i@wujingquan.com>
2019-04-30 23:57:07 +0800

    头部重构 完成

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <template>
-  <div id="app">
+  <div id="app" @touchmove.prevent>
     <v-header :seller="seller"></v-header>
-
-    <div class="tab border-1px">
-      <div class="tab-item">
-        <router-link to="/goods">商品</router-link>
-      </div>
-      <div class="tab-item">
-        <router-link to="/ratings">评论</router-link>
-      </div>
-      <div class="tab-item">
-        <router-link to="/seller">商家</router-link>
-      </div>
-    </div>
-    <keep-alive>
-      <router-view :seller="seller"></router-view>
-    </keep-alive>
   </div>
 </template>
 
 <script>
-import VHeader from './components/header/header'
+import VHeader from 'components/header/header'
 import { getSeller } from 'api'
 
 export default {
@@ -50,23 +35,6 @@ export default {
 }
 </script>
 
-<style lang="stylus">
-@import "./common/stylus/mixin"
+<style lang="stylus" scoped>
 
-#app
-  .tab
-    display flex
-    width 100%
-    height 40px
-    line-height 40px
-    border-1px(rgba(7, 17, 27, 0.1))
-    .tab-item
-      flex 1
-      text-align center
-      & > a
-        display block
-        font-size 14px
-        color rgb(77, 85, 93)
-        &.active
-          color rgb(240, 20, 20)
 </style>
